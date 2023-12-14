371ddf8a9a13971fb25296d8c9c6da2e86c72c84
obvPaul <Paul.Oppeneiger@student.htldornbirn.at>
2023-12-14 10:24:14 +0100

    JavaScript Basics | "TUTORIAL" DONE

---
+++ JavaScript/01 Variabeln_Operationen/script.js
@@ -0,0 +1,19 @@
+console.log("Hallo");
+console.log("Tolle Sache");
+
+// Variablen
+let a = 7;
+let b = 8;
+let c = a + b;
+
+console.log("A hat den Wert" + a);
+
+console.log("A hat den Wert" + a);
+
+console.log("a + b = " + c);
+
+// Rechnungsarten
+// *, /, +; -
+
+c = b - a;
+console.log("b - a " + c)
