c06ad058dfd5f3ff49ba4cf7f32f32c9b8fce7cf
obvPaul <Paul.Oppeneiger@student.htldornbirn.at>
2023-12-14 11:21:48 +0100

    JavaScript_Loops | END OF LESSON

--- JavaScript/04 Loops/script.js
+++ JavaScript/04 Loops/script.js
@@ -5,10 +5,14 @@ console.log("Tolle Sache");
 let a = 7;
 let b = 4.3;
 let name = "Andreas";
-let name10 = "Andreas mal 10";
 
 console.log(name + " " + b + " " + a);
 
 // Schleifen
 
+for (let count = 0;count<10;count++){
+    console.log(count + " Andreas")
+}
+
+console.log("Fertig");
 
