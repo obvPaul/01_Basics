6db16c3bf3af472901dfde30b0aaf931aed2bda0
obvPaul <Paul.Oppeneiger@student.htldornbirn.at>
2023-12-14 11:16:58 +0100

    JavaScript_Loops | START

---
+++ JavaScript/04 Loops/script.js
@@ -0,0 +1,14 @@
+console.log("Hallo");
+console.log("Tolle Sache");
+
+// Variablen
+let a = 7;
+let b = 4.3;
+let name = "Andreas";
+let name10 = "Andreas mal 10";
+
+console.log(name + " " + b + " " + a);
+
+// Schleifen
+
+
