80b63e92efe912e15e23a735e613ed14d8cea1cb
obvPaul <Paul.Oppeneiger@student.htldornbirn.at>
2023-12-14 10:49:40 +0100

    JavaScript_Strings & Numbers | "TUTORIAL" DONE

---
+++ JavaScript/02 Strings und Zahlen/script.js
@@ -0,0 +1,22 @@
+let a = 7;
+let b = 8;
+
+let result = a - b;
+
+console.log(result);
+console.log(result);
+
+// +, -, *, /
+
+let c = 7.4;
+let d = 7.5;
+
+result = c + d;
+console.log(result);
+
+// Text Strings
+
+let name = "Andreas";
+console.log(name);
+
+console.log("Das Ergebnis von " + c + " und " + d + " ist " + result);
