247236c261869ebb07eab5b15e7da9b9c5546b5c
obvPaul <Paul.Oppeneiger@student.htldornbirn.at>
2023-12-14 10:34:23 +0100

    JavaSprict_Basics | FINISHED

--- JavaScript/01 Variabeln_Operationen/script.js
+++ JavaScript/01 Variabeln_Operationen/script.js
@@ -16,4 +16,36 @@ console.log("a + b = " + c);
 // *, /, +; -
 
 c = b - a;
-console.log("b - a " + c)
+console.log("b - a = " + c);
+
+// Aufgaben
+
+a = 7;
+a = 8;
+a = 10;
+
+// Berechne folgendes und gib das Ergebnis über console.log aus
+
+// a + b + c
+console.log(a + b + c);
+// (a + b) / c
+console.log((a + b) / c);
+// c - a + b
+console.log(c - a + b);
+// a + b + c * 3
+console.log(a + b + c * 3);
+// (a + b + c) / 4
+console.log((a + b + c) / 4);
+
+// Ändere die Variablen auf folgende Werte
+// a = 3
+a = 3;
+// b = a + 3
+b = a + 3;
+// c = c - 3
+c = c - 3;
+
+//Gib die neuen Werte der Variablen aus
+console.log("Das neue c ist " + c);
+console.log("Das neue a ist " + a);
+console.log("Das neue b ist " + b);
