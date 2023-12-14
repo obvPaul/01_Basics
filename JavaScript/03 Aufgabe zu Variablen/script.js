bdfcb0e0264a68a9f29fa0f143531a6b1b7fce71
obvPaul <Paul.Oppeneiger@student.htldornbirn.at>
2023-12-14 10:57:43 +0100

    JavaScript_Task for Variables | START

---
+++ JavaScript/03 Aufgabe zu Variablen/script.js
@@ -0,0 +1,21 @@
+// Erstelle eine Variable banana mit dem Wert "Banane"
+
+// Erstelle eine Variable apple mit dem Wert "Apple"
+
+// Erstelle eine Variable bananaPricePerKilo mit dem Wert 2.14
+
+// Erstelle eine Variable applePricePerKilo mit dem Wert 3.43
+
+// Ein Apfel hat durchschnittlich ein Gewicht von 0.34 Kilo
+
+// Eine Banane hat durchschnittlich ein Gewicht von 0.22 Kilo
+
+// Gib folgendes aus: Anzahl Bananen/Äpfel pro Kilo, Preis pro Banane/Apfel
+
+// Preis von 8 Äpfeln
+
+// Preis von 17 Bananen
+
+// Preis von 1 Tonne Äpfel
+
+// Preis von 1 Tonne Bananen
