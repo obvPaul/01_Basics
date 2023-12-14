0a537ac3181ad7230ddff956545c17f97f4c1fe6
obvPaul <Paul.Oppeneiger@student.htldornbirn.at>
2023-12-14 10:53:57 +0100

    JavaScript_Strings & Numbers | FINISHED

--- JavaScript/02 Strings und Zahlen/script.js
+++ JavaScript/02 Strings und Zahlen/script.js
@@ -20,3 +20,18 @@ let name = "Andreas";
 console.log(name);
 
 console.log("Das Ergebnis von " + c + " und " + d + " ist " + result);
+
+// Aufgaben
+
+let e = 17.4;
+let f = 3.9;
+
+result = e / f;
+
+console.log("The result of " + e + " divided by " + f + " is " + result);
+
+
+let firstname = "Paul";
+let lastname = "Oppeneiger";
+
+console.log("Ich heiße " + firstname + " " + lastname);
