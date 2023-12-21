d1eed49dd2696e7a3a755a8ecbe66bd701d207c0
obvPaul <Paul.Oppeneiger@student.htldornbirn.at>
2023-12-21 10:22:10 +0100

    JavaScript_Loops | DEBUGGED_FINISHED

--- JavaScript/04 Loops/script.js
+++ JavaScript/04 Loops/script.js
@@ -10,8 +10,8 @@ console.log(name + " " + b + " " + a);
 
 // Schleifen
 
-for (let count = 10;count=>0;count--){
-    console.log(count + " Andreas")
+for (let count = 10;count>=0;count--){
+    console.log(count + "= Andreas")
 }
 
 console.log("Fertig");
