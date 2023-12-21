80d956e09358fc330f5f931bc532ab8f25ad7f2e
obvPaul <Paul.Oppeneiger@student.htldornbirn.at>
2023-12-21 10:17:02 +0100

    JavaScript_Loops | It started rosting every browser I try opening it with?

--- JavaScript/04 Loops/script.js
+++ JavaScript/04 Loops/script.js
@@ -10,9 +10,36 @@ console.log(name + " " + b + " " + a);
 
 // Schleifen
 
-for (let count = 10;count>0;count--){
+for (let count = 10;count=>0;count--){
     console.log(count + " Andreas")
 }
 
 console.log("Fertig");
 
+//  Zähle von 7 bis 45
+for (count = 7;count<=45;count++){
+    console.log(count)
+}
+
+console.log("Aufgabe 1 fertig!")
+
+// Zähle von 9 bis 99
+for (count = 9;count<=99;count++){
+    console.log(count)
+}
+
+console.log("Aufgabe 2  fertig!")
+
+// Zähle rückwärts von 10 bis 0
+for (count = 10;count>=0;count--){
+    console.log(count)
+}
+
+console.log("Aufgabe 3 fertig!")
+
+// Zähle rückwerts von 1234 bis 1207
+for (count = 1234;count>=1207;count--){
+    console.log(count)
+}
+
+console.log("Aufgabe 4 fertig!")
