cbe0909ceee288cc2300672da77f92db40eca1f8
obvPaul <Paul.Oppeneiger@student.htldornbirn.at>
2024-01-11 10:46:46 +0100

    JavaScript_If conditions | FINISHED

---
+++ JavaScript/05 If Bedingungen/script.js
@@ -0,0 +1,33 @@
+// erstelle eine Variable number
+
+let number = 44;
+
+// Aufgabe 1
+
+// Wenn die Zahl kleiner ist als 20  gib aus "Mini"
+if (number < 20) {
+  console.log("Mini");
+}
+// Wenn die Zahl zw. 20 und 50 ist gib aus "Medium"
+if (20 < number < 50) {
+  console.log("Medium");
+}
+// Wenn die Zahl größer als 50 ist gib aus "Large"
+if (number > 50) {
+  console.log("Large");
+}
+
+// Aufgabe 2
+let numberr = 21;
+// Wenn die Zahl kleiner gleich 7 ist gib aus "Mini"
+if (numberr <= 7) {
+  console.log("Mini");
+}
+// Wenn die Zahl zwischen 7 und 90 ist gib  aus "Medium"
+if (7 < numberr < 90) {
+  console.log("Mittel");
+}
+// Wenn die Zahl größer als 90 ist gib aus "Large"
+if (numberr > 90) {
+  console.log("Large");
+}
