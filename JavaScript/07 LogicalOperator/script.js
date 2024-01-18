94bd1313ff7dbe33009aa0c0d55a91882b8ee7c9
obvPaul <Paul.Oppeneiger@student.htldornbirn.at>
2024-01-18 11:16:15 +0100

    JavaScript_LogicalOperator | DEBUGGED_FINISHED

--- JavaScript/07 LogicalOperator/script.js
+++ JavaScript/07 LogicalOperator/script.js
@@ -1,26 +1,26 @@
 //Logische Operatoren
-if (41>7)||(3<4){
+if ((41>7)||(3<4)){
     console.log("Is true")
 }
 
 // Erstelle zwei Zufallszahl zwischen 0 und 100 
-let randomNumber = Math.random();
-let randomNumber2 = Math.random();
+let randomNumber = Math.random() * 100;
+let randomNumber2 = Math.random() * 100;
 
 let number = Math.floor(randomNumber);
-let number 2 = Math.floor(randomNumber2);
+let number2 = Math.floor(randomNumber2);
 
 // Speichere diese jeweils in einer Variable 
 /// ?
 
 // Wenn die erste Zahl kleiner ist als die zweite UND die erste Zahl kleiner ist als 50 dann gib aus "Zahl 1 ist kleiner als Zahl 2 und Mini" 
-if (number<number2)&&(number<50) {
+if ((number<number2)&&(number<50)) {
     console.log("Zahl 1 ist kleiner als Zahl 2 und Mini")
 }
 
 
 // Wenn die erste Zahl kleiner ist als 30 oder die zweite Zahl kleiner ist als 30 dann gib aus "Eine der beiden ist kleiner als 30" 
-if (number<30)||(number2<30) {
+if ((number<30)||(number2<30)) {
     console.log("Eine der beiden ist kleiner als 30")
 }
 
@@ -28,6 +28,6 @@ if (number<30)||(number2<30) {
  
 
 // Wenn die erste Zahl kleiner ist als 50 UND die zweite Zahl ungleich 50 ist dann gib aus "Erste Zahl klein, zweite kein 50iger" 
-if (number<50)&&(number2!=50) {
+if ((number<50)&&(number2!=50)) {
     console.log("Erste zahl klein, zweite kein 50er")
 }
