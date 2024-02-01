f69bcdd5db72595a6be70c016201a127538d6099
obvPaul <Paul.Oppeneiger@student.htldornbirn.at>
2024-02-01 11:27:52 +0100

    JavaScript_SwitchStatement | FINISHED

--- JavaScript/08 Switch Statement/script.js
+++ JavaScript/08 Switch Statement/script.js
@@ -1,11 +1,43 @@
 // Erstelle eine Zufallszahl zwischen 5 und 10
-let randomNumber = Math.random()
- 
+let randomNumber = Math.random() * 5 + 5;
+let number = Math.floor(randomNumber);
+
+console.log(number);
 
 // Wenn der Wert 10 ist gib aus Ten 
-
+if(number==10){
+    console.log("Ten")
+}
 // Wenn der Wert 9 ist gib aus Nine 
-
+if(number==9){
+    console.log("Nine")
+}
 // Wenn der Wert 8 ist gib aus Eight 
-
+if(number==8){
+    console.log("Eight")
+}
 // etc. 
+if(number==7){
+    console.log("Seven")
+}
+if(number==6){
+    console.log("Six")
+}
+if(number==5){
+    console.log("Five")
+}
+if(number==4){
+    console.log("Four")
+}
+if(number==3){
+    console.log("Three")
+}
+if(number==2){
+    console.log("Two")
+}
+if(number==1){
+    console.log("One")
+}
+if(number==0){
+    console.log("Zero")
+}
