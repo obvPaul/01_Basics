ec0304178488e12c33f235db794e187e16575878
obvPaul <Paul.Oppeneiger@student.htldornbirn.at>
2024-04-25 10:21:08 +0200

    SWP_Tests | Test_01 ROUGH FINISH

--- Tests/Test_01/script.js
+++ Tests/Test_01/script.js
@@ -1,20 +1,26 @@
-const data = "Hans geht heute schwimmen";
+const numbers = "8,2,1,4,6,1,8,7";
+// Zähle alle Zahlen die zwischen 4 (inklusive und 8 exklusive) liegen
+// Output: 3
 
-// Drehe die Wörter um
-// Output
-// schwimmen heute geht Hans
+let einzeln = numbers.split(",");
 
-let split [data];
-console.log(split)
+console.log(einzeln);
 
-const numbers = "1,2,1,4,6,8,7";
+let zähler = 0;
 
-//finde die kleinste Zahl und gib an wie oft sie vorkommt
-// Output: 1 2
+// Ich will 4, 5, 6 und 7 testen
 
-const temperatures = "-1,5,51,-10,-11,2,90,-8,-44";
+for (i = 0; i < einzeln.length; i++) {
+  let testen = einzeln[i];
+  if (testen == 4) {
+    zähler = zähler + 1;
+  } else if (testen == 5) {
+    zähler = zähler + 1;
+  } else if (testen == 6) {
+    zähler = zähler + 1;
+  } else if (testen == 7) {
+    zähler = zähler + 1;
+  }
+}
 
-// Berechne den Mittelwert der Temperaturen
-// Werte die größer gleich 50 und kleiner gleich -30 sind, sind -
-// - Außreißer und dürfen nicht berücksichtigt werden
-
+console.log(zähler);
